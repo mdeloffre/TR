4c9ca5e5b26bbfb550c4b5736427ca8e7baa1787
mdeloffre <matthieu.techno@gmail.com>
2020-05-07 09:43:28 +0200

    test

---
+++ make.sh
@@ -0,0 +1,8 @@
+cd ~/Bureau/TR/dumber/software/simulateur/dist/Debug/GNU-Linux
+gnome-terminal -- ./simulateur
+cd ~/Bureau/TR/dumber/software/monitor/monitor
+gnome-terminal -- ./monitor
+cd ~/Bureau/TR/dumber/software/raspberry/superviseur-robot/dist/Debug__PC_/GNU-Linux
+gnome-terminal -- sudo ./superviseur-robot
+
+
---
+++ makefile
@@ -0,0 +1,7 @@
+makefile:
+cd dumber/software/simulateur/dist/Debug/GNU-Linux/
+./simulateur &
+﻿cd dumber/software/monitor/monitor/
+./monitor &
+cd dumber/software/raspberry/superviseur-robot/dist/Debug__PC_/GNU-Linux/
+sudo ./superviseur-robot &
